6752e66cd56b7c3aad2935fa7ebd7d1fe026bfb3
ChethanShetty563 <chethan563@gmail.com>
2020-10-23 21:15:06 +0530

    Added the Controller logic to check the password criteria using passwordcheck() method

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -26,6 +26,8 @@ public class UserController {
     @Autowired
     private ImageService imageService;
 
+    String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+
     //This controller method is called when the request pattern is of type 'users/registration'
     //This method declares User type and UserProfile type object
     //Sets the user profile with UserProfile type object
@@ -42,7 +44,16 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
+    public String registerUser(User user, Model model) {
+        if(!passwordCheck(user.getPassword()))
+        {
+            UserProfile profile = new UserProfile();
+            user.setProfile(profile);
+            model.addAttribute("User", user);
+            model.addAttribute("passwordTypeError",error);
+            return "users/registration";
+        }
+
         userService.registerUser(user);
         return "redirect:/users/login";
     }
